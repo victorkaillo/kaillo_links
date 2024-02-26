34278f624e4950a1a1c84a124d45a2d4707d41f0
Victor Kaillo <victorkaillo@hotmail.com>
2024-02-26 09:03:36 -0300

    toggle used

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function togglePag() {
     // alert('Pagination');
     const html = document.documentElement;
-    // html.classList.toggle('light');
+    html.classList.toggle('light');
     const img = document.querySelector("#profile img");
     if (html.classList.contains('light')) {
-        html.classList.remove('light');
+        // html.classList.remove('light');
         img.setAttribute('src', './assets/avatar-vk.png');
     } else {
         img.setAttribute('src', './assets/avatar-vk-light.png');
-        html.classList.add('light');
+        // html.classList.add('light');
     }
 }
